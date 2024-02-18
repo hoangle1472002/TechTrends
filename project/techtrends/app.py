1098818a816083aa7edecaf3bf477404d0355013
hoangle1472002 <hoangle14702@gmail.com>
2024-02-18 17:26:14 +0700

    init

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,13 +1,23 @@
 import sqlite3
+import logging
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+# Variable to keep track of database connections
+db_connection_count = 0
+
+# Configure logging
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+
+
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global db_connection_count
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    db_connection_count += 1
     return connection
 
 # Function to get a post using its ID
@@ -25,6 +35,8 @@ app.config['SECRET_KEY'] = 'your secret key'
 # Define the main route of the web application 
 @app.route('/')
 def index():
+    # Log when the index page is accessed
+    logging.info('Index page accessed!')
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
@@ -36,14 +48,39 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        # Log when a non-existing article is accessed
+        logging.error(f'Non-existing article with ID {post_id} accessed!')
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        # Log when an existing article is retrieved
+        logging.info(f'Article "{post["title"]}" retrieved!')
+        return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    logging.info('About Us page accessed!')
     return render_template('about.html')
+
+# Define the /healthz endpoint
+@app.route('/healthz')
+def healthcheck():
+    response = app.response_class(
+        response=json.dumps({"result": "OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+# Define the /metrics endpoint
+@app.route('/metrics')
+def get_metrics():
+    connection = get_db_connection()
+    post_count = connection.execute('SELECT COUNT(*) FROM posts').fetchone()[0]
+    connection.close()
+
+    metrics = {"db_connection_count": db_connection_count, "post_count": post_count}
+    return jsonify(metrics), 200
 
 # Define the post creation functionality 
 @app.route('/create', methods=('GET', 'POST'))
@@ -60,6 +97,9 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
+
+            # Log when a new article is created
+            logging.info(f'New article "{title}" created!')
 
             return redirect(url_for('index'))
 
